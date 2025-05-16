879dd38721cf20733546481442a1ec4c583c6f29
汤乾华 <tqh@shushi.pro>
2025-05-16 09:48:28 +0800

    fix: 修改依赖

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,9 @@ import 'ant-design-vue/dist/antd.min.css';
 import 'element-plus/dist/index.css';
 import '@kunlun/vue-ui-antd/dist/kunlun-vue-ui-antd.css';
 import '@kunlun/vue-ui-el/dist/kunlun-vue-ui-el.css';
-import '@kunlun/designer-common/dist/kunlun-designer-common.css';
-import '@kunlun/model-designer/dist/kunlun-model-designer.css';
 
 import 'reflect-metadata';
 import { VueOioProvider } from '@kunlun/dependencies';
-
-// 设计器
-import '@kunlun/ui-designer-dependencies';
-import '@kunlun/model-designer';
 
 VueOioProvider({
   http: {
